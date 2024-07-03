4e89b946a40dde68556b10d50cd025239a78f0f6
Peter Absolon <peterabsolon@yahoo.com>
2024-07-03 12:06:38 +0200

    fix: add observer() to root page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,16 @@
+'use client'
+
+import { observer } from 'mobx-react-lite'
+
 import { Button } from '~/components'
 import { app } from '~/store'
 
-export default function Home() {
+const Home = observer(() => {
   return (
     <main>
       <Button onClick={() => app.toast.info('Sup')}>Sup</Button>
     </main>
   )
-}
+})
+
+export default Home
