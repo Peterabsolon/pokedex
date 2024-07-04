ee50c832692c5eed82f328a68afe33bf4261f0f5
Peter Absolon <peterabsolon@yahoo.com>
2024-07-04 12:26:29 +0200

    feat: basic page layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,10 +17,20 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={`${font.className} flex flex-col items-stretch justify-stretch bg-slate-950`}>
-        <header>Header</header>
         <Toaster richColors={true} position="top-right" />
+
+        <header className="flex items-center justify-between p-8">
+          <div className="flex items-center">
+            <img className="mr-4 w-12" alt="Pokeball logo" src="/pokeball.svg" />
+            <h1 className="font-medium">Pokedex</h1>
+          </div>
+        </header>
+
         <main className="z-20 flex flex-1 items-stretch justify-center overflow-auto px-8">{children}</main>
-        <header>Footer</header>
+
+        <footer className="absolute inset-0 top-auto z-20 p-8 text-right">
+          Made with 💜 by <a href="https://www.linkedin.com/in/peter-absolon">Peter Absolon</a>
+        </footer>
       </body>
     </html>
   )
