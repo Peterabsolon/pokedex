f94fc70d369c53a08ebe2f92a58038bb10dea083
Peter Absolon <peterabsolon@yahoo.com>
2024-07-08 08:04:39 +0200

    fix: 🔧 fetch info fragment only

--- frontend/src/graphql/getPokemons.query.ts
+++ frontend/src/graphql/getPokemons.query.ts
@@ -1,18 +1,15 @@
 import { gql } from '@apollo/client'
 
-import { POKEMON_DETAILS_FRAGMENT } from './fragments'
 import { POKEMON_INFO_FRAGMENT } from './fragments/pokemon-info.fragment'
 
 export const GET_POKEMONS_QUERY = gql`
   ${POKEMON_INFO_FRAGMENT}
-  ${POKEMON_DETAILS_FRAGMENT}
 
   query getPokemons($query: PokemonsQueryInput!) {
     pokemons(query: $query) {
       count
       edges {
         ...PokemonInfo
-        ...PokemonDetails
       }
     }
   }
