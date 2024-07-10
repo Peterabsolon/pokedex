b50ec5caaf4adab19475abf149be5514fdc097ed
Peter Absolon <peterabsolon@yahoo.com>
2024-07-10 10:57:14 +0200

    chore: delete TODO.md, fix buttons not being equal size

--- frontend/src/components/domain/PokemonActions/PokemonActions.tsx
+++ frontend/src/components/domain/PokemonActions/PokemonActions.tsx
@@ -17,7 +17,7 @@ export const PokemonActions = ({ pokemon, className, fixedWidth }: PokemonAction
   return (
     <div className={classNames('flex gap-2', className)}>
       <Button
-        className={classNames('flex-1', { 'px-0': fixedWidth })}
+        className={classNames('flex-1', { '!px-0': fixedWidth })}
         onClick={() => handleToggleFavorite(pokemon)}
         stopPropagation
         iconLeft={<HeartIcon className="mr-2 size-5" fill={pokemon.isFavorite ? 'currentColor' : 'none'} />}
@@ -26,7 +26,7 @@ export const PokemonActions = ({ pokemon, className, fixedWidth }: PokemonAction
       </Button>
 
       <Button
-        className={classNames('flex-1', { 'px-0': fixedWidth })}
+        className={classNames('flex-1', { '!px-0': fixedWidth })}
         onClick={() => handlePlaySound(pokemon)}
         stopPropagation
         iconLeft={<SpeakerIcon className="mr-2 size-5" />}
